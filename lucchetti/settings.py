c109fc851f49100444a0a26ce3f77d4d45c0e88c
Francesco Moggia FFG <fmoggia@fantasyflightgames.com>
2014-10-26 16:40:47 -0500

    Changed DB to mySQL

--- lucchetti/settings.py
+++ lucchetti/settings.py
@@ -124,14 +124,26 @@ LANGUAGES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django',
-        'USER': SECRETS['postgresql_username'],
-        'PASSWORD': SECRETS['postgresql_password'],
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'negozio',                      # Or path to database file if using sqlite3.
+        'USER': SECRETS['postgresql_username'],                      # Not used with sqlite3.
+        'PASSWORD': SECRETS['postgresql_password'],                  # Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+    },
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'django',
+#         'USER': SECRETS['postgresql_username'],
+#         'PASSWORD': SECRETS['postgresql_password'],
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
